b261f18a2e1280d4bbdc00b54595ef2636cda7c3
Dima <dbuyvid@gmail.com>
2022-02-26 19:22:58 +0300

    CP implementation

---
+++ src/main/java/by/bsuir/app/connection/ConnectionFactory.java
@@ -0,0 +1,66 @@
+package by.bsuir.app.connection;
+
+import by.bsuir.app.exception.ConnectionException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ConnectionFactory {
+    private final static String PROPERTIES_FILE_NAME = "db.properties";
+    private String url;
+    private String name;
+    private String password;
+
+    public BlockingQueue<ProxyConnection> createProxyConnections(int connectionsCount) throws
+            ConnectionException {
+        BlockingQueue<ProxyConnection> proxyConnections = new ArrayBlockingQueue<>(connectionsCount);
+        try {
+            readProperties();
+            for (int i = 0; i < connectionsCount; i++) {
+                proxyConnections.add(createProxyConnection());
+            }
+        } catch (ConnectionException | SQLException e) {
+            throw new ConnectionException(e.getMessage(), e);
+        }
+        return proxyConnections;
+    }
+
+    protected ProxyConnection createProxyConnection() throws SQLException {
+        return new ProxyConnection(createConnection());
+    }
+
+    protected Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(url, name, password);
+    }
+
+    protected Properties readProperties() throws ConnectionException {
+        Properties props = new Properties();
+        String driverName = null;
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME)) {
+
+            props.load(inputStream);
+            driverName = (props.getProperty("jdbc.driver"));
+            Class.forName(driverName);
+
+            url = props.getProperty("jdbc.url");
+            name = props.getProperty("jdbc.name");
+            password = props.getProperty("jdbc.password");
+
+            return props;
+        } catch (ClassNotFoundException e) {
+            throw new ConnectionException("Cannot find driver: " + driverName, e);
+        } catch (FileNotFoundException e) {
+            throw new ConnectionException("File doesn't exist: " + PROPERTIES_FILE_NAME, e);
+        } catch (IOException e) {
+            throw new ConnectionException(e.getMessage(), e);
+        }
+    }
+}
---
+++ src/main/java/by/bsuir/app/connection/ConnectionPool.java
@@ -0,0 +1,74 @@
+package by.bsuir.app.connection;
+
+import by.bsuir.app.exception.ConnectionException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConnectionPool {
+
+    private static final Logger LOGGER = LogManager.getLogger(ConnectionPool.class);
+    private static final int INITIAL_POOL_SIZE = 10;
+
+    private static ConnectionPool INSTANCE;
+    private final BlockingQueue<ProxyConnection> availableConnections;
+    private final BlockingQueue<ProxyConnection> connectionsInUse;
+
+    private static final Lock lock = new ReentrantLock();
+    private static final ConnectionFactory connectionFactory = new ConnectionFactory();
+
+    public ConnectionPool(BlockingQueue<ProxyConnection> availableConnections) {
+        this.availableConnections = availableConnections;
+        this.connectionsInUse = new ArrayBlockingQueue<>(INITIAL_POOL_SIZE);
+    }
+
+    public static ConnectionPool getInstance() {
+        ConnectionPool localInstance = INSTANCE;
+        if (localInstance == null) {
+            lock.lock();
+            localInstance = INSTANCE;
+            try {
+                if (localInstance == null) {
+                    BlockingQueue<ProxyConnection> proxyConnections = connectionFactory.createProxyConnections(
+                            INITIAL_POOL_SIZE);
+                    INSTANCE = localInstance = new ConnectionPool(proxyConnections);
+                }
+            } catch (ConnectionException e) {
+                LOGGER.error(e.getMessage(), e);
+            } finally {
+                lock.unlock();
+            }
+        }
+        return localInstance;
+    }
+
+    public void returnConnection(ProxyConnection proxyConnection) {
+        lock.lock();
+        try {
+            if (connectionsInUse.contains(proxyConnection)) {
+                availableConnections.add(proxyConnection);
+                connectionsInUse.remove(proxyConnection);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public ProxyConnection getConnection() {
+        lock.lock();
+        ProxyConnection connection = null;
+        try {
+                connection = availableConnections.take();
+                connectionsInUse.put(connection);
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            lock.unlock();
+        }
+        return connection;
+    }
+}
