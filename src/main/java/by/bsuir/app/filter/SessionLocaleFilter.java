892d5fc3623b1fd7593dd765af92c80b07bf61e6
Dima <dbuyvid@gmail.com>
2022-03-16 17:33:43 +0300

    added authFilter, charsetFilter

--- src/main/java/by/bsuir/app/filter/SessionLocaleFilter.java
+++ src/main/java/by/bsuir/app/filter/SessionLocaleFilter.java
@@ -5,14 +5,22 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class SessionLocaleFilter implements Filter {
+
+    private final static String LOCALE_NAME_ATTR = "lang";
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
 
-        String locale = req.getParameter("lang");
+        String locale = req.getParameter(LOCALE_NAME_ATTR);
         if (locale != null) {
-            req.getSession().setAttribute("lang", locale);
+            req.getSession().setAttribute(LOCALE_NAME_ATTR, locale);
         }
         chain.doFilter(request, response);
     }
+
+    @Override
+    public void destroy() {}
 }
